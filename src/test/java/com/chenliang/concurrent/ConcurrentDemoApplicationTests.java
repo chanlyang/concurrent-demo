12f825194224ce1a7b0fa3ee016b82346530599d
chenliang <ygglyang@gmail.com>
2020-04-13 16:52:25 +0800

    Initial commit

---
+++ src/test/java/com/chenliang/concurrent/ConcurrentDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenliang.concurrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
